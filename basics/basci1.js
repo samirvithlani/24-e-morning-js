bec92f5c5c90c13ff0d454587e3b110ee894d939
samirvithlani <samir.vithlani83955@gmail.com>
2024-08-21 09:52:43 +0530

    basic,variable

---
+++ basics/basci1.js
@@ -0,0 +1,31 @@
+//header...
+//main....
+//printf("message");
+console.log("Hello World");
+
+var a =10 //int
+//printf("%d",a)
+//printf("value of a = %d",a)
+console.log(a);
+console.log("value of a",a)
+//type of operator
+console.log("type of a = ",typeof a)
+
+var pers = 89.9 //float
+console.log("value of pers = ",pers);
+console.log("type of pers = ",typeof pers)
+
+
+
+var uname = "ram"//char
+console.log("value of uname = ",uname);
+console.log("type of uname = ",typeof uname)
+
+
+var available = true //boolean
+console.log("value of available = ",available);
+console.log("type of available = ",typeof available)
+
+var x; //undefined
+console.log("value of x = ",x);
+console.log("type of x = ",typeof x)
