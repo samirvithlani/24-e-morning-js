9ce7646871ce9417187ea18024b9a5919ca5ec55
samirvithlani <samir.vithlani83955@gmail.com>
2024-10-02 10:56:54 +0530

    sub string

---
+++ string/string8.js
@@ -0,0 +1,8 @@
+var data = "hi this is India";
+
+// 0 1 2 -->exclusive
+///var ss = data.substring(0,4)//   0 1 2 3
+//var ss = data.substring(7)
+//var ss = data.slice(0,4) // 0 1 2 3
+var ss = data.slice(7)
+console.log(ss); //hi 
