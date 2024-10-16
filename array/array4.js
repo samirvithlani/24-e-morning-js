45bd03a8081ec54b4b4392f9930b76d2e49f30e5
samirvithlani <samir.vithlani83955@gmail.com>
2024-10-16 10:56:42 +0530

    splice

---
+++ array/array4.js
@@ -0,0 +1,10 @@
+var users = ["amit","kunal","sachin","rahul"];
+//1st param will index position
+//2nd param will number of elements to remove
+console.log(users)
+//users.splice(1,2)
+//users.splice(2,1)
+//users.splice(1,1,"ajay")
+//users.splice(1,1,"ajay","vijay","jay")
+users.splice(1,0,"ajay")
+console.log(users)
