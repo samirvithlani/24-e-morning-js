919e5aa0934829bbe28e68f29c0c3f8f49e4f04e
samirvithlani <samir.vithlani83955@gmail.com>
2024-11-20 09:35:26 +0530

    funciton 1

---
+++ array/array5.js
@@ -0,0 +1,6 @@
+// var sales = [20,22,33,44,20,50]
+// //salesWithTax =[22,24.2,36.3,47.4,22,55]
+
+
+var users = ["ram","shyam","amit","kunal","sahil","sagar"]
+//usersWithMr = ["Mr. ram","Mr. shyam","Mr. amit","Mr. kunal","Mr. sahil","Mr. sagar"]
---
+++ functions/function1.js
@@ -0,0 +1,31 @@
+//without return type without arguments | param
+
+function test(){
+
+    console.log("test function called...")
+    //logic...
+}
+
+function drawLine(){
+    console.log("_____________________________________________")
+}
+
+test() // calling part..
+drawLine()
+console.log("hello")
+drawLine()
+test();
+drawLine()
+console.log("hi")
+drawLine()
+
+
+function sum(){
+
+    let a = 10,b=20,c=0;
+    c = a + b;
+    console.log("ans = ",c)
+
+
+}
+sum()
