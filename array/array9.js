2fe6a1139af1a524341654d8b7b198290a7f45c0
samirvithlani <samir.vithlani83955@gmail.com>
2024-12-11 09:25:41 +0530

    filter

---
+++ array/array9.js
@@ -0,0 +1,51 @@
+var sales = [100,120,123,119,102,234]
+var filSales =[];
+
+for(let i=0;i<sales.length;i++){
+
+    //100 t
+    //120 t
+    //123 f
+    if(sales[i]%2==0){
+        filSales.push(sales[i])
+    }
+
+}
+console.log(filSales)
+
+//s == sales[i]
+var filSales2 = sales.filter((s)=>{
+    return s %2 == 0; //only predicate statement
+})
+console.log(filSales2)
+
+var cities = ["ahmedabad","aurangabad","pune","firozabad","alhabad","mumbai"]
+
+var filCities =[]
+
+for(let i=0;i<cities.length;i++){
+
+    if(cities[i].endsWith("abad")){
+        filCities.push(cities[i])
+    }
+}
+
+console.log(filCities)
+
+var filcities2 = cities.filter((c)=>{
+    return c.endsWith("abad")
+})
+console.log(filCities)
+
+
+
+
+
+
+
+
+
+
+
+
+
