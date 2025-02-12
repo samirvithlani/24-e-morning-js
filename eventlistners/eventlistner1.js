ccdfa8580225fb71858d5941f25823e5df70b5b7
samirvithlani <samir.vithlani83955@gmail.com>
2025-02-12 19:52:01 +0530

    circle listners

--- eventlistners/eventlistner1.js
+++ eventlistners/eventlistner1.js
@@ -1,11 +1,33 @@
-const btn = document.getElementById("button") //<button>
-btn.addEventListener("click",()=>{
-    btn.innerHTML = "clicked"
-    btn.disabled = true
-})
-btn.addEventListener("mouseenter",()=>{
-    btn.style.backgroundColor = "grey"
-})
-btn.addEventListener("mouseleave",()=>{
-    btn.style.backgroundColor = "white"
-})
+const btn = document.getElementById("button"); //<button>
+btn.addEventListener("click", () => {
+  btn.innerHTML = "clicked";
+  btn.disabled = true;
+});
+btn.addEventListener("mouseenter", () => {
+  btn.style.backgroundColor = "grey";
+});
+btn.addEventListener("mouseleave", () => {
+  btn.style.backgroundColor = "white";
+});
+
+var colorList = ["red", "green", "yellow", "blue", "grey", "pink"];
+const mydiv = document.getElementById("mydiv");
+mydiv.addEventListener("mouseenter", () => {
+  //random index
+
+  var randomIndex = Math.floor(Math.random() * colorList.length); //3
+  mydiv.style.backgroundColor = colorList[randomIndex];
+
+  const rect = mydiv.getBoundingClientRect();
+  console.log("X Position:", rect.left);
+  console.log("Y Position:", rect.top);
+
+  rect.left = 609.6000366210938 
+  rect.top = 29.200000762939453
+  //609.6000366210938 
+  //29.200000762939453
+
+
+
+
+});
