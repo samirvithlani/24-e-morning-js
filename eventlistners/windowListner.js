795231ded2971e4b581009145e0687d24ae1c22a
samirvithlani <samir.vithlani83955@gmail.com>
2025-02-19 20:10:11 +0530

    window

---
+++ eventlistners/windowListner.js
@@ -0,0 +1,20 @@
+window.addEventListener("DOMContentLoaded",()=>{
+//    alert("html loadded...")
+})
+
+// window.addEventListener("copy",()=>{
+//     alert("copy...")
+// })
+
+// window.addEventListener("paste",()=>{
+//     alert("paste")
+// })
+
+// window.addEventListener("scroll",()=>{
+
+//     console.log("scrolling...")
+// })
+
+window.addEventListener("resize",()=>{
+    console.log("resized window...")
+})
